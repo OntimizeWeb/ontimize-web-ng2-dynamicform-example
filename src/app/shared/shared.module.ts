61e4d613c1edbc33a9afde22e2f3aa3fb774d51d
gonzalo.martinez <gonzalo.martinez@imatia.com>
2017-09-27 18:43:42 +0200

    Deleting unnecesary component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,19 +2,15 @@ import { NgModule } from '@angular/core';
 import { OntimizeWebModule } from 'ontimize-web-ngx';
 import { CommonModule } from '@angular/common';
 
-import { BarMenuComponent } from './menu/bar-menu.component';
-
 @NgModule({
   imports: [
     CommonModule,
     OntimizeWebModule
   ],
   declarations: [
-    BarMenuComponent
   ],
   exports: [
-    CommonModule,
-    BarMenuComponent
+    CommonModule
   ]
 })
 export class SharedModule { }
