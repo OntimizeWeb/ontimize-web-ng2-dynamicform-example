570cdb82ad54e46ec63e36e65359638b61763099
gonzalo.martinez <gonzalo.martinez@imatia.com>
2018-04-02 15:32:20 +0200

    Updating dependencies

--- src/app/main/dynamicforms/new/dynamicforms-new.form.component.ts
+++ src/app/main/dynamicforms/new/dynamicforms-new.form.component.ts
@@ -1,34 +1,25 @@
-import {
-  Injector,
-  forwardRef,
-  NgZone,
-  ChangeDetectorRef,
-  ElementRef
-} from '@angular/core';
-
-import {
-  Router,
-  ActivatedRoute
-} from '@angular/router';
-
-import {
-  OComponent,
-  OFormComponent
-} from 'ontimize-web-ngx';
-
+import { Injector, forwardRef, NgZone, ChangeDetectorRef, ElementRef, Component, ViewEncapsulation } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { OComponent, OFormComponent, OntimizeService, dataServiceFactory } from 'ontimize-web-ngx';
 import { ODynamicFormBuilderComponent } from 'ontimize-web-ngx-dynamicform-builder';
 
-@OComponent({
+@Component({
   selector: 'dynamicforms-new-form',
   providers: [
+    { provide: OntimizeService, useFactory: dataServiceFactory, deps: [Injector] },
     {
       provide: OFormComponent,
       useExisting: forwardRef(() => DynamicFormsNewFormComponent)
     }
   ],
-  inputs: [
-    ...OFormComponent.DEFAULT_INPUTS_O_FORM
-  ]
+  templateUrl: './o-form.component.html',
+  inputs: OFormComponent.DEFAULT_INPUTS_O_FORM,
+  outputs: OFormComponent.DEFAULT_OUTPUTS_O_FORM,
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    '[class.o-form]': 'true',
+    '[class.fill]': 'layoutFill'
+  }
 })
 export class DynamicFormsNewFormComponent extends OFormComponent {
 
